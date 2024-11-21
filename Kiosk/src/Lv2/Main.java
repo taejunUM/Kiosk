c0d928925f96d701c647207d6cd09fb595c77245
taejunUM <utj0411@naver.com>
2024-11-21 21:04:36 +0900

    modify gitignore

--- Kiosk/src/Lv2/Main.java
+++ Kiosk/src/Lv2/Main.java
@@ -8,14 +8,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-//        String[] menus = {
-//                "1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거",
-//                "2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거",
-//                "3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거",
-//                "4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거",
-//                "0. 종료           | 종료"
-//        };
-
+        /**
+         * 햄버거 메뉴 클래스로 관리하기
+         * 햄버거 메뉴를 List로 저장하여 관리
+         */
 
         ArrayList<MenuItem> menuItems = new ArrayList<>();
         menuItems.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
@@ -23,6 +19,9 @@ public class Main {
         menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
         menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
 
+        /**
+         * 햄버거 메뉴 출력 및 입력값에 따라 프로그램 종료
+         */
 
         while (true) {
             System.out.println("[ SHAKESHACK MENU ]");
